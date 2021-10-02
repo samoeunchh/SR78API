4bcbb03b592f1c3ff44e974d99883466b586fa51
samoeun chhay <samoeunchh@gmail.com>
2021-10-02 08:20:10 +0700

    Delete data

--- SR78.PresentationDesktop/Form1.Designer.cs
+++ SR78.PresentationDesktop/Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace SR78.PresentationDesktop
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -44,9 +52,9 @@ namespace SR78.PresentationDesktop
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(-5, 280);
+            this.listView1.Location = new System.Drawing.Point(-5, 196);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(979, 232);
+            this.listView1.Size = new System.Drawing.Size(979, 316);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -65,16 +73,82 @@ namespace SR78.PresentationDesktop
             this.columnHeader3.Text = "Description";
             this.columnHeader3.Width = 543;
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(324, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(324, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Brand Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(975, 509);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,6 +158,13 @@ namespace SR78.PresentationDesktop
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SR78.PresentationDesktop/Form1.cs
+++ SR78.PresentationDesktop/Form1.cs
@@ -33,12 +33,73 @@ namespace SR78.PresentationDesktop
             if (response.IsSuccessStatusCode)
             {
                 var brands =await response.Content.ReadAsAsync<List<Brand>>();
+                if (brands == null)
+                {
+                    MessageBox.Show("No record");
+                    return;
+                }
                 foreach(var item in brands)
                 {
                     var li = listView1.Items.Add(item.BrandId.ToString());
                     li.SubItems.Add(item.BrandName);
                     li.SubItems.Add(item.Description);
                 }
+            }
+            else
+            {
+                var msg = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                MessageBox.Show(msg);
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Brand Name is required");
+                return;
+            }
+            var brand = new Brand
+            {
+                BrandName = textBox1.Text,
+                Description = textBox2.Text
+            };
+            HttpResponseMessage response = await client.PostAsJsonAsync<Brand>("api/brands", brand);
+            if (response.IsSuccessStatusCode)
+            {
+                Form1_Load(sender, e);
+            }
+            else
+            {
+                var msg = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                MessageBox.Show(msg);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var text = "Do you want to delete this record?";
+                DialogResult result = MessageBox.Show(text, "Delete",MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    var id = listView1.SelectedItems[0].Text;
+                    HttpResponseMessage response = await client.DeleteAsync("api/brands/"+ id);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Form1_Load(sender, e);
+                    }
+                    else
+                    {
+                        var msg = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                        MessageBox.Show(msg);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an item for deleting...");
             }
         }
     }
